84473dd06171353d3a4e6f6559f3894d04d257cd
Roberto Rodriguez Castro <ing_roca@outlook.com>
2025-07-07 23:01:25 -0700

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,13 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'tp4j6k1k',
+    dataset: 'production'
+  },
+  /**
+   * Enable auto-updates for studios.
+   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+   */
+  autoUpdates: true,
+})
